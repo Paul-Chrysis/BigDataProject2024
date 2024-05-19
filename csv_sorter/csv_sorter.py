6a0c497bd0e7be01bcdde4805d54856ec751f84d
Paul-Chrysis <xrushspaulos@gmail.com>
2024-05-19 19:55:30 +0300

    mend

--- csv_sorter/csv_sorter.py
+++ csv_sorter/csv_sorter.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-# Read the CSV file into a DataFrame
 df = pd.read_csv('./../sources/vehicles.csv')
-
-# Sort the DataFrame based on a specific field, for example, 't'
 df_sorted = df.sort_values(by='t')
-
-# Write the sorted DataFrame back to a CSV file
 df_sorted.to_csv('./../sources/sorted_vehicles.csv', index=False)
