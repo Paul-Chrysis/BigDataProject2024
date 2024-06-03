bfc1a89676a7230e8664d485abf9c4952e109046
Paul-Chrysis <xrushspaulos@gmail.com>
2024-06-03 21:56:20 +0300

    [FEATURE] Created spark container that reads data from kafka, does some computations and stores the data back to kafka | Created docker-compose file | Updated README.md

---
+++ message_consumer/message_consumer.py
@@ -0,0 +1,32 @@
+from kafka import KafkaConsumer, TopicPartition
+from kafka.errors import KafkaError
+
+bootstrap_servers = 'bdp-kafka-container:9092'
+topic_name = 'vehicle_positions'
+
+def consume():
+    consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers,
+                             auto_offset_reset='earliest')
+    
+    partitions = consumer.partitions_for_topic(topic_name)
+    if partitions is None:
+        print(f"Topic '{topic_name}' not found.")
+        return
+
+    try:
+        for partition in partitions:
+            topic_partition = TopicPartition(topic_name, partition)
+            consumer.assign([topic_partition])
+            consumer.seek_to_beginning(topic_partition)
+            for message in consumer:
+                print(f"Consumed message from partition {partition}: {message.value.decode('utf-8')}")
+    except KafkaError as e:
+        print(f"Error consuming messages: {e}")
+    except KeyboardInterrupt:
+        print("Stopping consumer...")    
+    finally:
+        consumer.close()
+        print("Consumer closed.")   
+
+if __name__ == "__main__":
+    consume()
